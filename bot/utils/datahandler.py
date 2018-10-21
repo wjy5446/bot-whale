02f60f2b34a1ad4ed349e6d66486e45396ecb1f3
wjy5446 <whaledev.yoon@gmail.com>
2018-10-21 21:40:55 +0900

    feat: add the datahandler

---
+++ bot/utils/datahandler.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+
+
+class DataHandler(object):
+	def __init__(self):
+		self.data_path = "data/"
+		self.log_data_path = "log/data/"
+
+	def read_file(self, filename):
+		text = self.read_text(filename)
+		if text == "":
+			return {}
+		else:
+			return json.loads(text)
+
+	def read_text(self, filename, filepath):
+		if filepath is None:
+			filepath = self.data_path
+		path = os.path.join(filepath + filename)
+		try:
+			with open(path, "rb") as infile:
+				return infile.read().decode('utf-8')
+		except BaseException:
+			return ""
+
+	def write_file(self, filename, data):
+		path = os.path.join(self.data_path + filename)
+		with open(path, "w", encoding="utf-8") as outfile:
+			json.dump(data, outfile)
+		
