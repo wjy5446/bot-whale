550fdfb58df6a425dc9d1459f715e6bef268df40
wjy5446 <whaledev.yoon@gmail.com>
2018-10-27 12:27:17 +0900

    feat: add function log

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -71,5 +71,41 @@ class MessageLogger(object):
 	def get_logger(self):
 		return self.instance._logger
 
+class FunctionLogger(object):
+	class __Logger:
+		def __init__(self):
+			self._logger = logging.getLogger("function")
+			self._logger.setLevel(logging.INFO)
+			formatter = logging.Formatter(
+				"%(asctime)s > %(message)s"
+			)
+
+			now = datetime.datetime.now()
+
+			dir_name = "./log/function"
+			if not os.path.isdir(dir_name):
+				os.makedirs(dir_name)
+
+			fileHandler = logging.FileHandler(
+				dir_name + "/" + now.strftime("%Y-%m-%d") + ".log"
+			)
+
+			fileHandler.setFormatter(formatter)
+			self._logger.addHandler(fileHandler)
+
+	instance = None
+
+	def __init__(self):
+		if not FunctionLogger.instance:
+			FunctionLogger.instance = FunctionLogger.__Logger()
+
+	def get_logger(self):
+		return self.instance._logger
+
+
+
+
+
+
 
 
